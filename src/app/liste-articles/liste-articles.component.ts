1146205d1efbfd40bf59d68a28227d7cb4d2e4fb
Anida Assane Yacouba <aniyacoub18@gmail.com>
2023-11-29 15:18:04 +0000

    erreur volontaire

--- src/app/liste-articles/liste-articles.component.ts
+++ src/app/liste-articles/liste-articles.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import {DataService} from "../data.service";
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-liste-articles',
   templateUrl: './liste-articles.component.html',
-  styleUrls: ['./liste-articles.component.css']
+  styleUrls: ['./liste-articles.component.css'],
 })
 export class ListeArticlesComponent {
   // @ts-ignore
   liste;
-  constructor(private dataService: DataService) {
-  }
-  ngOnInit(){
-    this.liste=this.dataService.liste;
+  constructr(private dataService: DataService) {}
+  ngOnInit() {
+    this.liste = this.dataService.liste;
   }
 }
